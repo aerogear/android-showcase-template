af1a13bdc7ac7b7ca4efcc91da674cd9873b52a7
rachael-oregan <roregan@redhat.com>
2018-05-23 16:15:38 +0100

    refactor to use new push module

--- app/src/main/java/com/aerogear/androidshowcase/features/push/presenters/PushPresenter.java
+++ app/src/main/java/com/aerogear/androidshowcase/features/push/presenters/PushPresenter.java
@@ -4,6 +4,8 @@ import com.aerogear.androidshowcase.features.push.views.PushView;
 import com.aerogear.androidshowcase.mvp.presenters.BasePresenter;
 import org.aerogear.mobile.core.Callback;
 import org.aerogear.mobile.core.MobileCore;
+import org.aerogear.mobile.core.executor.AppExecutors;
+import org.aerogear.mobile.core.reactive.Responder;
 import org.aerogear.mobile.push.PushService;
 import org.aerogear.mobile.push.UnifiedPushConfig;
 import java.util.Arrays;
@@ -23,17 +25,18 @@ public class PushPresenter extends BasePresenter<PushView> {
 
     public void unregister() {
         PushService pushService = MobileCore.getInstance().getService(PushService.class);
-        pushService.unregisterDevice(new Callback() {
-            @Override
-            public void onSuccess() {
-                view.unregisterSuccess();
-            }
+        pushService.unregisterDevice().respondOn(new AppExecutors().mainThread())
+            .respondWith(new Responder<Boolean>() {
+                @Override
+                public void onResult(Boolean value) {
+                    view.unregisterSuccess();
+                }
 
-            @Override
-            public void onError(Throwable error) {
-                view.unregisterError(error);
-            }
-        });
+                @Override
+                public void onException(Exception exception) {
+                    view.unregisterError(exception);
+                }
+            });
     }
 
     public void register() {
@@ -42,17 +45,18 @@ public class PushPresenter extends BasePresenter<PushView> {
         unifiedPushConfig.setCategories(Arrays.asList("Android", "Example"));
 
         PushService pushService = MobileCore.getInstance().getService(PushService.class);
-        pushService.registerDevice(unifiedPushConfig, new Callback() {
-            @Override
-            public void onSuccess() {
-                view.registerSuccess();
-            }
+        pushService.registerDevice().respondOn(new AppExecutors().mainThread())
+            .respondWith(new Responder<Boolean>() {
+                @Override
+                public void onResult(Boolean value) {
+                    view.registerSuccess();
+                }
 
-            @Override
-            public void onError(Throwable error) {
-                view.registerError(error);
-            }
-        });
+                @Override
+                public void onException(Exception exception) {
+                    view.registerError(exception);
+                }
+            });
     }
 
     public void refreshToken() {
