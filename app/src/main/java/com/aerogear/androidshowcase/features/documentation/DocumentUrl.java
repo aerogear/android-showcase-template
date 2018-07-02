82549ab86cbb3022609f69382d413c54d195c775
Jason Madigan <jason@jasonmadigan.com>
2018-07-02 14:12:29 +0100

    AEROGEAR-3564 - use #nochrome link to hide navbar on docs

--- app/src/main/java/com/aerogear/androidshowcase/features/documentation/DocumentUrl.java
+++ app/src/main/java/com/aerogear/androidshowcase/features/documentation/DocumentUrl.java
@@ -4,12 +4,12 @@ package com.aerogear.androidshowcase.features.documentation;
  * This class enumerates the documentation pages we have.
  */
 public enum DocumentUrl {
-    PUSH("https://docs.aerogear.org/aerogear/latest/showcase/push.html"),
-    IDENTITY_MANAGEMENT("https://docs.aerogear.org/aerogear/latest/showcase/idm.html"),
-    DEVICE_SECURITY("https://docs.aerogear.org/aerogear/latest/showcase/device-security.html"),
-    METRICS("https://docs.aerogear.org/aerogear/latest/showcase/metrics.html"),
-    RUNTIME("https://docs.aerogear.org/aerogear/latest/showcase/runtime.html"),
-    IDENTITY_MANAGEMENT_SSO("https://docs.aerogear.org/aerogear/latest/keycloak/index.html?sso=1"),
+    PUSH("https://docs.aerogear.org/aerogear/latest/showcase/push.html#nochrome"),
+    IDENTITY_MANAGEMENT("https://docs.aerogear.org/aerogear/latest/showcase/idm.html#nochrome"),
+    DEVICE_SECURITY("https://docs.aerogear.org/aerogear/latest/showcase/device-security.html#nochrome"),
+    METRICS("https://docs.aerogear.org/aerogear/latest/showcase/metrics.html#nochrome"),
+    RUNTIME("https://docs.aerogear.org/aerogear/latest/showcase/runtime.html#nochrome"),
+    IDENTITY_MANAGEMENT_SSO("https://docs.aerogear.org/aerogear/latest/keycloak/index.html?sso=1#nochrome"),
     NOTES_SERVICE("https://github.com/feedhenry/mobile-security/tree/master/projects/api-server");
 
     private final String url;
